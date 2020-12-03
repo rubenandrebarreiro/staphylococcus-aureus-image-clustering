2479bdd677e61b571f90ce760e9f1bb78e0d254c
Rúben André Barreiro <r.barreiro@campus.fct.unl.pt>
2020-12-03 10:27:41 +0000

    Some minor changes (Features Extraction process done)
    Some minor changes (Features Extraction process done)

--- TP2.py
+++ TP2.py
@@ -9,7 +9,7 @@
 # -------------------------------------------------------------------#
 
 
-# Libraries Used:
+# A) Libraries Used:
 
 # - 1) General Libraries:
 
@@ -18,7 +18,10 @@
 # as load_txt
 from numpy import loadtxt as load_txt
 
-from numpy import savetxt as save_txt
+# Import zeros,
+# From the NumPy's Python Library,
+# as matrix_array_zeros
+from numpy import zeros as matrix_array_zeros
 
 
 # - 2) Customised Libraries:
@@ -32,7 +35,6 @@ from tp2_aux import images_as_matrix as images_as_numpy_matrix
 # From the TP2_Aux Customised Python Library,
 # as html_report_cluster_labels
 from tp2_aux import report_clusters as html_report_cluster_labels
-
 
 
 # - 3) Libraries for Features Extraction:
@@ -56,23 +58,25 @@ from sklearn.manifold import TSNE as t_distributed_stochastic_neighbor_embedding
 from sklearn.manifold import Isomap as isometric_mapping 
 
 
+# - 4) Libraries for Scoring Performance:
 
-# TODO - Confirmar
 # Import Function Classifier,
 # From the Feature Selection Module
 # of the SciKit-Learn's Python Library,
-# as function_classifier
-from sklearn.feature_selection import f_classif as f_classifier
+# as f_1_score
+from sklearn.feature_selection import f_classif as f_1_score
 
 
 # - 5) Clustering Methods' Libraries:
 
 # Import cluster.KMeans Sub-Module,
-# from SciKit-Learn Python's Library as k_means
+# from SciKit-Learn Python's Library,
+# as k_means
 from sklearn.cluster import KMeans as k_means
 
 # Import cluster.DBSCAN Sub-Module,
-# from SciKit-Learn Python's Library as dbscan
+# from SciKit-Learn Python's Library,
+# as dbscan
 from sklearn.cluster import DBSCAN as dbscan
 
 
@@ -80,11 +84,13 @@ from sklearn.cluster import DBSCAN as dbscan
 # - 6) Scoring/Metrics' Libraries:
 
 # Import cluster.KMeans Sub-Module,
-# from SciKit-Learn Python's Library as k_means
+# from SciKit-Learn Python's Library,
+# as k_means
 from sklearn.metrics import adjusted_rand_score as skl_adjusted_rand_score
 
 # Import cluster.DBSCAN Sub-Module,
-# from SciKit-Learn Python's Library as dbscan
+# from SciKit-Learn Python's Library,
+# as dbscan
 from sklearn.metrics import silhouette_score as skl_silhouette_score
 
 
@@ -92,28 +98,39 @@ from sklearn.metrics import silhouette_score as skl_silhouette_score
 # -------------------------------------------------------------------#
 
 
+# B) CONSTANTS:
+    
 NUM_FEATURES_COMPONENTS = 6
 
 
 # -------------------------------------------------------------------#
 
 
+# C) PROGRAM:
+
+    
+# Step 1:
+# - Initialise the global variables and prepare the data;
+
+  
 # The 2D NumPy Matrix, representing all the samples images,
 # with an image per row (563 lines),
 # and one indiviual pixel by column
 # (50 pixels x 50 pixels = 2500 columns)
-xs_images_matrix = images_as_numpy_matrix(N = 563)
+xs_images_matrix = images_as_numpy_matrix()
 
-# Just for Debug
-# save_txt(fname="images_matrix.txt", X=xs_images_as_numpy_matrix, delimiter=", ", newline="\n")
 
+# The Total Number of Images/Examples
+num_total_images_examples = len(xs_images_matrix)
+
+
+# The Identifiers and Labels for
+# the Examples and their Celular Phases of
+# the samples of Staphycoccus Aureus,
+# provided by Super-Resolution Fluorescence Microscopy Photographies
 ids_and_labels = load_txt("labels.txt", delimiter=",")
 
-
-# Just for Debug
-# print(ids_and_labels)
-
-# The Labels for Celular Phases of the samples of Staphycoccus Aureus,
+# The Labels for the Celular Phases of the samples of Staphycoccus Aureus,
 # provided by Super-Resolution Fluorescence Microscopy Photographies
 ys_labels = ids_and_labels[:,1]
 
@@ -133,37 +150,65 @@ xs_ids_not_labelled_data = xs_ids[ys_labels == 0]
 xs_ids_labelled_data = xs_ids[ys_labels != 0]
 
 
-# TODO - Juntar tudo numa tabela, xs da matriz da imagem com os ys das labels ???
+
+# Step 2:
+# - Process of Features' Extraction;
 
 
-# For each method of Features Extraction
+# For each method of Features' Extraction
 # (3 methods of Extraction of Features),
 # extract 6 Features (a total of 3 x 6 = 18 features):
-# - 1) PCA (Principal Component Analysis) Decomposition;
-# - 2) TSNE (T-Distributed Stochastic Neighbor Embedding);
-# - 3) Isomap (Isometric Mapping);
+
+# - 1) PCA (Principal Component Analysis) Decomposition:
 pca = pca_decomposition(n_components = NUM_FEATURES_COMPONENTS)
 
+# - 2) TSNE (T-Distributed Stochastic Neighbor Embedding):
+tsne = t_distributed_stochastic_neighbor_embedding(n_components = NUM_FEATURES_COMPONENTS, method = "exact")
 
-# TODO - confirmar components = features ???
-tsne = t_distributed_stochastic_neighbor_embedding(n_components = NUM_FEATURES_COMPONENTS)
-
-
-isomap = isometric_mapping(n_components = 6)
+# - 3) Isomap (Isometric Mapping):
+isomap = isometric_mapping(n_components = NUM_FEATURES_COMPONENTS)
 
 
 # Fit the PCA (Principal Component Analysis) Decomposition,
-# with the 2D NumPy Matrix, representing all the images
+# with the 2D NumPy Matrix, representing all the images,
+# and transform the respective data, for the Feature Extraction
 pca.fit(xs_images_matrix)
-
-
 transformed_xs_images_matrix_pca = pca.transform(xs_images_matrix)
 
-
+# Fit the TSNE (T-Distributed Stochastic Neighbor Embedding),
+# with the 2D NumPy Matrix, representing all the images,
+# and transform the respective data, for the Feature Extraction
 transformed_xs_images_matrix_tsne = tsne.fit_transform(xs_images_matrix)
 
-
+# Fit the Isomap (Isometric Mapping),
+# with the 2D NumPy Matrix, representing all the images,
+# and transform the respective data, for the Feature Extraction
 transformed_xs_images_matrix_isomap = isomap.fit_transform(xs_images_matrix)
+
+
+# The final Features Extracted, to be used, in the Clustering methods,
+# filled initially with zeros (0s)
+xs_features = matrix_array_zeros( (num_total_images_examples, ( 3 * NUM_FEATURES_COMPONENTS ) ) )
+
+# The final Features Extracted, to be used, in the Clustering methods,
+# filled with the fitted and transformed data,
+# from the PCA (Principal Component Analysis) Decomposition
+xs_features[:, 0 : NUM_FEATURES_COMPONENTS] = transformed_xs_images_matrix_pca
+
+# The final Features Extracted, to be used, in the Clustering methods,
+# filled with the fitted and transformed data,
+# from the TSNE (T-Distributed Stochastic Neighbor Embedding)
+xs_features[:, NUM_FEATURES_COMPONENTS : ( 2 * NUM_FEATURES_COMPONENTS ) ] = transformed_xs_images_matrix_tsne
+
+# The final Features Extracted, to be used, in the Clustering methods,
+# filled with the fitted and transformed data,
+# from the Isomap (Isometric Mapping)
+xs_features[:, ( 2 * NUM_FEATURES_COMPONENTS ) : ( 3 * NUM_FEATURES_COMPONENTS ) ] = transformed_xs_images_matrix_isomap
+
+
+
+# Step 2:
+# - Process of Clustering;
 
 
 # k clusters
@@ -174,18 +219,15 @@ e = 10
 
 for num_clusters in range(k):
 
-
-    
-    
     k_means_clustering = k_means(num_clusters)
             
-    k_means_clustering.fit(xs_images_matrix)
+    k_means_clustering.fit(xs_features)
     
     k_means_clustering_predicted_clusters_labels = k_means_clustering.predict(xs_images_matrix)
     
     centroids = k_means_clustering.cluster_centers_
     
-    # TODO - como variar a distancia da vizinhanca e ????        
+    
     for neighborhood_distance in range(e):
     
     
@@ -196,5 +238,7 @@ for num_clusters in range(k):
         
         dbscan(algorithm=...)
         
-        
-        
+
+
+
+# -------------------------------------------------------------------#
